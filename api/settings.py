81bb715e470ff250263971d38a19146f5ef770c1
Gnusarov Vlad <gnusarovvladislav@gmail.com>
2024-03-20 16:23:43 +0300

    Добавление CORS Добавление CORS origins для клиенткого приложения react

--- api/settings.py
+++ api/settings.py
@@ -46,16 +46,22 @@ INSTALLED_APPS = [
     'djoser',
     'users',
     'django_db_logger',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
 ]
 
 ROOT_URLCONF = 'api.urls'
